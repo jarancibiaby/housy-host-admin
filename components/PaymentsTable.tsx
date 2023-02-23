86a104a50dbf73f339dbd79002b39803a0b3dde1
Rodrigo Funes Biasatti <rfunes@hexacta.com>
2023-02-23 16:49:40 -0300

    ADD: mock data payments

--- components/PaymentsTable.tsx
+++ components/PaymentsTable.tsx
@@ -42,30 +42,105 @@ const columns = [
   },
 ];
 
+const mockPayments: Payment[] = [
+  {
+    id: '100',
+    customId: '200',
+    project: 'Proyect0',
+    amount: 1000,
+    date: '01/01/2023'
+  },
+  {
+    id: '101',
+    customId: '201',
+    project: 'Proyect1',
+    amount: 1000,
+    date: '01/02/2023'
+  },
+  {
+    id: '102',
+    customId: '202',
+    project: 'Proyect2',
+    amount: 1000,
+    date: '01/03/2023'
+  },
+  {
+    id: '104',
+    customId: '204',
+    project: 'Proyect4',
+    amount: 1000,
+    date: '01/05/2023'
+  },
+  {
+    id: '105',
+    customId: '205',
+    project: 'Proyect5',
+    amount: 1000,
+    date: '01/06/2023'
+  },
+  {
+    id: '106',
+    customId: '206',
+    project: 'Proyect6',
+    amount: 1000,
+    date: '01/07/2023'
+  },
+  {
+    id: '107',
+    customId: '207',
+    project: 'Proyect7',
+    amount: 1000,
+    date: '01/08/2023'
+  },
+  {
+    id: '108',
+    customId: '208',
+    project: 'Proyect8',
+    amount: 1000,
+    date: '01/09/2023'
+  },
+  {
+    id: '109',
+    customId: '209',
+    project: 'Proyect9',
+    amount: 1000,
+    date: '01/10/2023'
+  },
+  {
+    id: '110',
+    customId: '210',
+    project: 'Proyect10',
+    amount: 1000,
+    date: '01/11/2023'
+  },
+]
+
 export default function PaymentsTable() {
   const [payments, setPayments] = useState([] as Payment[]);
   const [selectionModel, setSelectionModel] = useState([] as string[]);
 
   useEffect(() => {
-    getAuth().onAuthStateChanged((user) => {
-      const q = query(
-        collection(db, "payments"),
-        where("uid", "==", user?.uid),
-        orderBy("date", "desc")
-      );
+    // getAuth().onAuthStateChanged((user) => {
+    //   const q = query(
+    //     collection(db, "payments"),
+    //     where("uid", "==", user?.uid),
+    //     orderBy("date", "desc")
+    //   );
 
-      onSnapshot(q, (querySnapshot) => {
-        setPayments(
-          querySnapshot.docs.map(
-            (doc: any, index: number): Payment => ({
-              ...doc.data(),
-              id: doc.id,
-              customId: (index += 1),
-            })
-          )
-        );
-      });
-    });
+    //   onSnapshot(q, (querySnapshot) => {
+    //     setPayments(
+    //       querySnapshot.docs.map(
+    //         (doc: any, index: number): Payment => ({
+    //           ...doc.data(),
+    //           id: doc.id,
+    //           customId: (index += 1),
+    //         })
+    //       )
+    //     );
+    //   });
+    // });
+
+    setPayments(mockPayments);
   }, []);
 
   const deleteRows = () => {
