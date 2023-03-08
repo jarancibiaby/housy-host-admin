6da52c26a70c8cb03245f11033ed4bdefcbe74ee
juanarancibia <arancibia.juan22@gmail.com>
2023-03-08 10:59:32 -0300

    delete env and files

--- models/payment.model.ts
+++
@@ -1,9 +0,0 @@
-import { Timestamp } from "@firebase/firestore";
-
-export interface Payment {
-  id?: string;
-  customId: string;
-  project: string;
-  amount: number;
-  date: Date | string | Timestamp;
-}
